f82a8cfecbe7ab46cb6c98c8b9ac227db364a8e5
poyanpayandeh <81269820+poyanpayandeh@users.noreply.github.com>
2023-05-27 22:57:41 +1200

    yess

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ let currentDay = String(date.getDate()).padStart(2, "0");
 let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
 let currentYear = date.getFullYear();
 console.log(date);
+
+document.getElementById("submit").addEventListener("click", submit);
+
+function submit() {
+    console.log("submit");
+}
