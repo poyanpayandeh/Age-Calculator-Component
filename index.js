3c65d9a523685aea68e21a8136d64700b4ea8156
poyanpayandeh <poyanpayandeh@hotmail.com>
2023-05-27 22:08:57 +1200

    lotta stuff

---
+++ index.js
@@ -0,0 +1,6 @@
+let dateElement = document.getElementById("date");
+const date = new Date();
+let currentDay = String(date.getDate()).padStart(2, "0");
+let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
+let currentYear = date.getFullYear();
+console.log(date);
