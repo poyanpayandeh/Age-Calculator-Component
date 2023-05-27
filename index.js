6d591e7e6310c968656ae8eee0da3963723b7717
poyanpayandeh <81269820+poyanpayandeh@users.noreply.github.com>
2023-05-28 01:41:01 +1200

    added error text

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ let currentMonth = String(date.getMonth() + 1).padStart(2, "0");
 let currentYear = date.getFullYear();
 console.log(date);
 
+// event listen for button
 document.getElementById("submit").addEventListener("click", submit);
+
 
 function submit() {
     console.log("submit");
